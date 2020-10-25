49e1606286f2eab404df1ea8b43738a6e8a475af
Marco <aeglon97@gmail.com>
2020-10-25 14:40:33 -0400

    Define global window variables

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,12 @@
 #include <iostream>
 #include "simulator.h"
 
+#define SCREENWIDTH 1000
+#define SCREENHEIGHT 680
+
 int main()
 {
-    Simulator simulator(1000, 680);
+    Simulator simulator(SCREENWIDTH, SCREENHEIGHT);
     simulator.Loop();
     return 0;
 }
